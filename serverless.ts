b2f78ac3dafed3f297e8e379a64a4c80f8ed72e3
g-barbosa <giovanne.bbarbosa@gmail.com>
2022-01-28 21:11:01 -0300

    feat: create saveUsers handler and add connection with dynamodb

--- serverless.ts
+++ serverless.ts
@@ -3,10 +3,11 @@ import type { AWS } from '@serverless/typescript';
 const serverlessConfiguration: AWS = {
   service: 'serveless',
   frameworkVersion: '2',
-  plugins: ['serverless-esbuild', 'serverless-offline'],
+  plugins: ['serverless-esbuild', 'serverless-dynamodb-local','serverless-offline'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    region: "us-east-1",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
@@ -16,15 +17,20 @@ const serverlessConfiguration: AWS = {
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
     lambdaHashingVersion: '20201221',
+    iamRoleStatements: [{
+      Effect: "Allow",
+      Action: ["dynamodb:*"],
+      Resource: ["*"]
+    }]
   },
   // import the function via paths
   functions: { 
-    hello: {
-      handler: "src/functions/hello.handler",
+    saveUsers: {
+      handler: "src/functions/saveUsers.handler",
       events: [{
         http: {
-          path: "hello",
-          method: "get",
+          path: "saveUsers",
+          method: "post",
           cors: true
         }
       }]
@@ -42,7 +48,41 @@ const serverlessConfiguration: AWS = {
       platform: 'node',
       concurrency: 10,
     },
+    dynamodb: {
+      stages: ['dev', 'local'],
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true
+      }
+    }
   },
+  resources: {
+    Resources: {
+      dbCertificateUsers: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "users",
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5
+          },
+          AttributeDefinitions: [
+            {
+              AttributeName: "id",
+              AttributeType: "S"
+            }
+          ],
+          KeySchema: [
+            {
+              AttributeName: "id",
+              KeyType: "HASH"
+            }
+          ]
+        }
+      }
+    }
+  }
 };
 
 module.exports = serverlessConfiguration;
